a285bc76fad1f7a9cf5e35a6e151fa952380bcd5
alevy <andrei.levchenko.ie@gmail.com>
2025-03-23 23:55:18 +0000

    Changes in UserController

--- backend/src/main/java/ie/gti/asdl/rey/gtirecord/backend/controller/UserController.java
+++ backend/src/main/java/ie/gti/asdl/rey/gtirecord/backend/controller/UserController.java
@@ -4,9 +4,12 @@ import ie.gti.asdl.rey.gtirecord.model.User;
 import ie.gti.asdl.rey.gtirecord.core.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Profile("web")
 @RestController
@@ -19,9 +22,21 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user")
-    public User getUser(@RequestParam Integer id) {
-        return userService.getUserById(id).orElse(null);
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable Integer id) {
+        return userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found")).getBody();
+//        userService.getUserById(id).orElse(null);
     }
+
+    @GetMapping("/user/{username}")
+    public User getUser(@PathVariable String username) {
+        return userService.getUserByUsername(username)
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found")).getBody();
+    }
+
+
 
 }
