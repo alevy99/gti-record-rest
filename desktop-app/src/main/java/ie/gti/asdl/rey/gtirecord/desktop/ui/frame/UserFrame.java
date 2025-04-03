49366f22ca1548d2e8501863bdd063ff21cd5795
alevy <andrei.levchenko.ie@gmail.com>
2025-04-03 22:38:54 +0100

    cleanup

--- desktop-app/src/main/java/ie/gti/asdl/rey/gtirecord/desktop/ui/frame/UserFrame.java
+++ desktop-app/src/main/java/ie/gti/asdl/rey/gtirecord/desktop/ui/frame/UserFrame.java
@@ -397,10 +397,6 @@ public class UserFrame extends AbstractTableDataFrame<User> {
         }
         PersonFrame personFrame = getFrameManager().getFrame(PERSON);
 
-        // Lazy insert person to user
-//        userService.insertPersonToUser(selectedUser);
-
-//        personFrame.setPersonId(selectedUser.getPersonId());
         personFrame.setUser(selectedUser);
         getFrameManager().showSub(PERSON);
     }
