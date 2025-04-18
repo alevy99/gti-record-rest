f30e24c868aaf256a8d376390350899af9aa12db
alevy <andrei.levchenko.ie@gmail.com>
2025-04-19 00:54:51 +0100

    add Web prrogile

--- backend/src/main/java/ie/gti/asdl/rey/gtirecord/backend/controller/TeacherController.java
+++ backend/src/main/java/ie/gti/asdl/rey/gtirecord/backend/controller/TeacherController.java
@@ -3,6 +3,7 @@ package ie.gti.asdl.rey.gtirecord.backend.controller;
 import ie.gti.asdl.rey.gtirecord.core.service.TeacherService;
 import ie.gti.asdl.rey.gtirecord.model.entity.Teacher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 /**
  * @author Andrei Levchenko
  */
+@Profile("web")
 @RestController
 public class TeacherController {
 
