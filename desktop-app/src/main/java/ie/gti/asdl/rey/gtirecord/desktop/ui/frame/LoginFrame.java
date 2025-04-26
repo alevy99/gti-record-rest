5d64ffd1b8ca0b69ed8cb4dbb853c43ee94f75cc
alevy <andrei.levchenko.ie@gmail.com>
2025-04-26 23:47:44 +0100

    Improve Login frame UI

--- desktop-app/src/main/java/ie/gti/asdl/rey/gtirecord/desktop/ui/frame/LoginFrame.java
+++ desktop-app/src/main/java/ie/gti/asdl/rey/gtirecord/desktop/ui/frame/LoginFrame.java
@@ -4,6 +4,7 @@
  */
 package ie.gti.asdl.rey.gtirecord.desktop.ui.frame;
 
+import java.awt.*;
 import java.awt.event.*;
 import java.util.Optional;
 import javax.swing.*;
@@ -31,8 +32,29 @@ public class LoginFrame extends AbstractFrame {
         userService = serviceManager.getUserService();
         initComponents();
         initFrame();
+        init();
+    }
+    
+    
+    private void init() {
+        resizeLabelIcon();
     }
 
+    private void resizeLabelIcon() {
+        Icon gretbIcon = lblGretb.getIcon(); // myLabel — это твой JLabel
+        if (gretbIcon instanceof ImageIcon imageIcon) {
+            Image image = imageIcon.getImage();
+            Image scaledImage = image.getScaledInstance(216, 84, Image.SCALE_SMOOTH);
+            lblGretb.setIcon(new ImageIcon(scaledImage));
+        }
+
+        Icon gtiIcon = lblGti.getIcon(); // myLabel — это твой JLabel
+        if (gtiIcon instanceof ImageIcon imageIcon) {
+            Image image = imageIcon.getImage();
+            Image scaledImage = image.getScaledInstance(152, 120, Image.SCALE_SMOOTH);
+            lblGti.setIcon(new ImageIcon(scaledImage));
+        }
+    }
 
     /**
      * This method is called from within the constructor to initialize the form. WARNING: Do NOT modify this code. The
@@ -45,10 +67,12 @@ public class LoginFrame extends AbstractFrame {
         jUsernameTF = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
+        lblTitle = new javax.swing.JLabel();
         jLoginBtn = new javax.swing.JButton();
         jPasswordTF = new javax.swing.JPasswordField();
         jExitBtn = new javax.swing.JButton();
+        lblGti = new javax.swing.JLabel();
+        lblGretb = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -60,13 +84,21 @@ public class LoginFrame extends AbstractFrame {
 
         jUsernameTF.setText("johnm");
 
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 51, 204));
         jLabel1.setText("Username");
 
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(0, 51, 204));
         jLabel2.setText("Password");
 
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel3.setText("GTI RECORD SYSTEM");
+        lblTitle.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        lblTitle.setForeground(new java.awt.Color(0, 51, 204));
+        lblTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblTitle.setText("GTI RECORD SYSTEM");
 
+        jLoginBtn.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLoginBtn.setForeground(new java.awt.Color(0, 51, 204));
         jLoginBtn.setText("Login");
         jLoginBtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -90,6 +122,8 @@ public class LoginFrame extends AbstractFrame {
             }
         });
 
+        jExitBtn.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jExitBtn.setForeground(new java.awt.Color(0, 51, 204));
         jExitBtn.setText("Exit");
         jExitBtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -97,49 +131,58 @@ public class LoginFrame extends AbstractFrame {
             }
         });
 
+        lblGti.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/gti-logo.png"))); // NOI18N
+
+        lblGretb.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/gretb-logo.png"))); // NOI18N
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(147, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(35, 35, 35)
+                .addComponent(lblGti)
+                .addGap(33, 33, 33)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jExitBtn, javax.swing.GroupLayout.DEFAULT_SIZE, 154, Short.MAX_VALUE)
-                    .addComponent(jLoginBtn, javax.swing.GroupLayout.DEFAULT_SIZE, 154, Short.MAX_VALUE)
-                    .addComponent(jUsernameTF, javax.swing.GroupLayout.DEFAULT_SIZE, 154, Short.MAX_VALUE)
-                    .addComponent(jPasswordTF))
-                .addGap(195, 195, 195))
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup()
-                    .addGap(183, 183, 183)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 224, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(183, Short.MAX_VALUE)))
+                    .addComponent(jExitBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLoginBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jUsernameTF)
+                    .addComponent(jPasswordTF, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+                .addComponent(lblGretb, javax.swing.GroupLayout.PREFERRED_SIZE, 247, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+            .addComponent(lblTitle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(158, 158, 158)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jUsernameTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jPasswordTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(26, 26, 26)
-                .addComponent(jLoginBtn)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
-                .addComponent(jExitBtn)
-                .addGap(30, 30, 30))
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup()
-                    .addGap(75, 75, 75)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(258, Short.MAX_VALUE)))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(76, Short.MAX_VALUE)
+                .addComponent(lblTitle, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(lblGti))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(28, 28, 28)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(jUsernameTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel1))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(jLabel2)
+                                    .addComponent(jPasswordTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(26, 26, 26)
+                                .addComponent(jLoginBtn))
+                            .addComponent(lblGretb, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(58, 58, 58)
+                        .addComponent(jExitBtn)))
+                .addGap(106, 106, 106))
         );
 
         pack();
@@ -188,10 +231,12 @@ public class LoginFrame extends AbstractFrame {
     private javax.swing.JButton jExitBtn;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JButton jLoginBtn;
     private javax.swing.JPasswordField jPasswordTF;
     private javax.swing.JTextField jUsernameTF;
+    private javax.swing.JLabel lblGretb;
+    private javax.swing.JLabel lblGti;
+    private javax.swing.JLabel lblTitle;
     // End of variables declaration//GEN-END:variables
 
     @Override
