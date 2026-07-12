f4a33d78bf97e4d5ec72acc94b19315cdcbc8694
alevy <andrei.levchenko.ie@gmail.com>
2026-07-12 22:54:10 +0100

    Add JavaDoc to GroupController

--- backend/src/main/java/ie/gti/asdl/rey/gtirecord/backend/controller/GroupController.java
+++ backend/src/main/java/ie/gti/asdl/rey/gtirecord/backend/controller/GroupController.java
@@ -11,20 +11,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
 /**
+ * REST controller exposing endpoints for retrieving {@link Group} information.
+ * <p>
+ * This controller is only active when the {@code "web"} Spring profile is
+ * enabled, and delegates all data access to {@link GroupService}.
+ *
  * @author Andrei Levchenko
  */
 @Profile("web")
 @RestController
 public class GroupController {
 
+    /** Service used to retrieve group data. */
     private final GroupService groupService;
 
+    /**
+     * Creates a new {@code GroupController} with the given group service.
+     *
+     * @param groupService the service used to look up groups
+     */
     @Autowired
     public GroupController(GroupService groupService) {
         this.groupService = groupService;
     }
 
+    /**
+     * Retrieves all groups.
+     *
+     * @return a list of all {@link Group} records
+     */
     @GetMapping("/groups")
     public List<Group> getGroups() {
         return groupService.getAll();
